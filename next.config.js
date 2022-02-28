dc732f25c76f71e42514c2c22b63750bdda67285
Ash Muhamad <legacy1194@gmail.com>
2022-02-28 23:45:14 +0800

    Removed NextJs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "/portfolio",
-  basePath: "/portfolio",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
