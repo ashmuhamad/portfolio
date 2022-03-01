3056ee7ec17e48e8efb107869c5e2ab1c7b93fcd
Ash Muhamad <legacy1194@gmail.com>
2022-03-01 19:24:30 +0800

    Added metatags

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,6 +4,13 @@ const Header = () => {
     <>
       <Head>
         <title>Ash Muhamad | Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to my portfolio. Coded with love, Ash."
+        />
+        <meta property="og:title" content="Ash Muhamad | Portfolio" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
   );
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useContext } from "react";
-import logo from "../../public/Ash.png";
+import logo from "../../public/images/logo.png";
 import { MouseContext } from "../../contexts/MouseContextProvider";
 
 const Navbar = () => {
