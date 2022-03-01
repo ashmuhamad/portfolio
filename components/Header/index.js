18a51f392e9dd218b0c6e226dd95586b023653d1
Ash Muhamad <legacy1194@gmail.com>
2022-03-01 20:06:53 +0800

    Updated metatags

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,8 +7,24 @@ const Header = () => {
         <meta
           name="description"
           content="Welcome to my portfolio. Coded with love, Ash."
+          key="title"
         />
-        <meta property="og:title" content="Ash Muhamad | Portfolio" />
+        <meta
+          property="og:title"
+          content="Ash Muhamad | Portfolio"
+          key="title"
+        />{" "}
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio. Coded with love, Ash."
+          key="description"
+        />
+        <meta
+          property="og:url"
+          content="https://ashportfolio.vercel.app/"
+          key="url"
+        />
+        <meta property="og:locale" content="en_US" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
